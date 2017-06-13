21a57803b4e1920cee98d58ab494e03e8eacf0f1
ostrander-nathan <ostrander_nathan@yahoo.com>
2017-06-13 10:42:03 -0500

    nav bar installed

--- NashvillesNextNeighborhood/app/HomeController.js
+++ NashvillesNextNeighborhood/app/HomeController.js
@@ -32,7 +32,7 @@ app.controller("HomeController",
                 title: "Click to zoom"
             });
 
-            $scope.getNashData = function() {
+//            $scope.getNashData = function() {
                 //console.log("Get Nash Data Function");
                 $.ajax({
                     url: "https://data.nashville.gov/resource/p5r5-bnga.json",
@@ -65,15 +65,42 @@ app.controller("HomeController",
                             var permitLocations = permitIssued[y];
                         }
                         var location = new google.maps.LatLng(permitIssued.mapped_location.coordinates[1], permitIssued.mapped_location.coordinates[0]);
+           //             var contentString = '<div id="content">' +
+           //'<div id="siteNotice">' +
+           //'</div>' +
+           //'<h1 id="firstHeading" class="firstHeading">{permitIssued.address}</h1>' +
+           //'<div id="bodyContent">' +
+           //'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+           //'sandstone rock formation in the southern part of the ' +
+           //'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+           //'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+           //'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+           //'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+           //'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+           //'Aboriginal people of the area. It has many springs, waterholes, ' +
+           //'rock caves and ancient paintings. Uluru is listed as a World ' +
+           //'Heritage Site.</p>' +
+           //'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+           //'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+           //'(last visited June 22, 2009).</p>' +
+           //'</div>' +
+           //'</div>';
+
+                        //var infowindow = new google.maps.InfoWindow({
+                        //    content: contentString
+                        //});
                         var marker = new google.maps.Marker({
                             position: location,
                             title: permitIssued.address,
                             map: $scope.map
                         });
+                        //marker.addListener('click', function () {
+                        //    infowindow.open(map, marker);
+                        //});
 
                     }
                 });
-            };
+//            };
 
             $scope.loadMore = function(data) {
                 console.log("LoadMore function");
